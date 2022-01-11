41f567c13014f8d1e30d9c25e045ca94b4911dc5
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-11 18:56:41 -0500

    assertions to check if two arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -7,13 +7,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if(!array1 || !array2) {
+  if (!array1 || !array2) {
     return "Pass two arrays as arguments";
   }
   if (array1.length !== array2.length) {
     return false;
   }
-  for(let i = 0; i < array1.length; i++) {
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
       continue;
     } else {

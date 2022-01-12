2a8e9c73684e0741be0191c87763b12d4c682145
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-11 20:28:10 -0500

    remove items from source array

---
+++ without.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (!array1 || !array2) {
+    return "Pass two arrays as arguments";
+  }
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let output = [];
+  const itemsSet = new Set(itemsToRemove);
+  for (let element of source) {
+    if (!itemsSet.has(element)) {
+      output.push(element);
+    }
+  }
+  return output;
+};
+
+
+assertArraysEqual(without([1,2,3], [1]), [2,3]);
+assertArraysEqual(without(["1","2","3"], ["3", 4,5]), ["1","2"]);
+assertArraysEqual(without([1,2,3], []), [1,2,3]);
+
+

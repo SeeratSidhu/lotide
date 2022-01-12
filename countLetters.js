c397a3858c5f8b2a0a9c86ff47d82a4201d91bee
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-12 15:50:58 -0500

    returns count of each letter in a sentence

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let counts = {};
+  let str = string.split(' ').join('').toLowerCase();
+
+  for (const letter of str) {
+    if (counts[letter]) {
+      counts[letter] += 1;
+    } else {
+      counts[letter] = 1;
+    }
+  }
+
+  return counts;
+};
+
+const result = countLetters("lighthouse labs");
+
+assertEqual(result["l"], 2);
+assertEqual(result["s"], 2);
+assertEqual(result["t"], 1);

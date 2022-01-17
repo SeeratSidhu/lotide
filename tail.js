af83e10d6169a99e6a422a28cff5345d701fdea7
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-17 14:37:25 -0500

    adding test cases for tail.js using mocha and chai

--- tail.js
+++ tail.js
@@ -1,4 +1,7 @@
 const tail = function(array) {
+  if(!array || !Array.isArray(array)) {
+    return;
+  }
   let result = [];
   if (array.length > 1) {
     for (let i = 1; i < array.length; i++) {
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,30 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const { expect } = require('chai');
 
-const random = ["Hello", "You", "Are", "Awesome"];
-const randomTail = tail(random);
-
-assertEqual(randomTail.length, 3);
-assertEqual(randomTail[0], "You");
-assertEqual(randomTail[1], "Are");
-assertEqual(randomTail[2], "Awesome");
-assertEqual(random.length, 4);
+describe('#tail', () => {
+  it("should return [2,3] for [1,2,3]", () => {
+    const result = tail([1,2,3]);
+    const expectedResult = [2,3];
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it("should return ['is', 'snowing'] for ['it', 'is', 'snowing']", () => {
+      const result = tail(['it', 'is', 'snowing']);
+      expect(result).to.deep.equal(['is', 'snowing']);
+  });
+  it("should return undefined if there is no array", () => {
+    expect(tail()).to.equal(undefined);
+  });
+  it("should return undefined if argument is not an array", () => {
+    expect(tail("hello")).to.equal(undefined);
+  });
+  it("should return length as 4 for [1,2,3,4]", function() {
+    const originalArray = [1,2,3,4]
+    const result = tail(originalArray);
+    expect(originalArray.length).to.equal(4);
+  });
+  it("should return 'you' for the first element of tail of ['Hello', 'you', 'are', 'awesome']", () => {
+    const originalArray = ['Hello', 'you', 'are', 'awesome'];
+    const result = tail(originalArray);
+    expect(result[0]).to.equal('you');
+  });
+});

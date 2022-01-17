8bc1453ccbc281e6603a96ca4af622bcf5b54a2f
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-17 14:01:22 -0500

    added test cases for head using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,23 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const { assert, expect } = require('chai');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["hi"]), "hi");
+describe('#head', () => {
+  it("returns 1 for [1,2,3", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+  it("return '5' for ['5']", () => {
+    expect(head(['5'])).to.equal('5');
+  });
+  it("should return 'hi' for ['hi', 'Bob']", () => {
+    expect(head(['hi', 'Bob'])).to.equal('hi');
+  });
+  it("should return undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("should return undefined if there is no array", () => {
+    assert.strictEqual(head(), undefined);
+  });
+  it("should return undefined if argument is not an array", () => {
+    assert.strictEqual(head("hello"), undefined);
+  });
+});

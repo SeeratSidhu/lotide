7d0072899337bcb6c3f28397302b7a8ffe37ea89
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-17 19:47:23 -0500

    modularize eqObject

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (!array1 || !array2 || array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const eqObjects = function(object1, object2) {
   let ob1Keys = Object.keys(object1);
@@ -53,4 +36,6 @@ eqObjects(cd, cd2); // => false
 
 assertEqual(eqObjects(cd, dc), false);
 assertEqual(eqObjects(cd, cd2), true);
+
+module.exports = eqObjects;
   
